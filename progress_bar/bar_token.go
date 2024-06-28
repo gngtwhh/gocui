80824084324966c0071985baf02861822bfb850b
gngtwhh <2784126850@qq.com>
2024-06-28 18:03:38 +0800

    feat: ProgressBar支持自定义样式
    提供对格式Token的解析支持，通过指定style字符串来定制进度条的样式

---
+++ progress_bar/bar_token.go
@@ -0,0 +1,137 @@
+package progress_bar
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Param is an alias for the empty interface and is used to give the toString method any type of parameter value.
+type Param = interface{}
+
+// Token is the interface that all tokens must implement.
+type Token interface {
+	toString(p ...Param) string
+}
+
+// Here are the tokens that can be used in the format string.
+// All tokens use the toString method to convert the tokens to a string for print.
+
+type TokenBar struct{}
+type TokenCurrent struct{}
+type TokenTotal struct{}
+type TokenPercent struct{}
+type TokenElapsed struct{}
+type TokenRate struct{}
+type TokenString struct {
+	payload string
+}
+
+// toString prints the bar.
+// p should be left int,right int,block string
+// return " "*left + block + " "*right
+func (b *TokenBar) toString(p ...Param) string {
+	return strings.Repeat(" ", p[0].(int)) + p[2].(string) + strings.Repeat(" ", p[1].(int))
+}
+
+// p should be current int
+// return "%d" of current
+func (c *TokenCurrent) toString(p ...Param) string {
+	return strconv.Itoa(p[0].(int))
+}
+
+// p should be total int
+// return "%d" of total
+func (t *TokenTotal) toString(p ...Param) string {
+	return strconv.Itoa(p[0].(int))
+}
+
+// p should be current int,total int
+// return "dd.dd%"
+func (t *TokenPercent) toString(p ...Param) string {
+	var percent float64
+	if p[0].(int) == 0 {
+		percent = 0
+	} else {
+		percent = float64(p[0].(int)) / float64(p[1].(int)) * 100
+	}
+	// 保留2位小数
+	return fmt.Sprintf("%5.2f%%", percent)
+}
+
+// p should be time.Duration
+// return "dd.dds"
+func (t *TokenElapsed) toString(p ...Param) string {
+	return fmt.Sprintf("%5.2fs", p[0].(time.Duration).Seconds())
+}
+
+// p should be time.Duration
+// return "dd.d ops/s"
+func (t *TokenRate) toString(p ...Param) string {
+	// ops/s
+	count := float64(time.Second) / float64(p[0].(time.Duration))
+	return fmt.Sprintf("%4.1f ops/s", count)
+}
+
+// p should be void
+// just return the payload of the string token
+func (s *TokenString) toString(p ...Param) string {
+	return s.payload
+}
+
+// unmarshalToken converts the token string to a slice of tokens.
+func unmarshalToken(token string) (ts []Token) {
+	if len(token) == 0 {
+		return
+	}
+	legalTokens := []string{
+		"%bar", "%current", "%total", "%percent", "%elapsed", "%rate",
+	}
+	for len(token) > 0 {
+		if token[0] != '%' {
+			if idx := strings.IndexAny(token, "%"); idx == -1 {
+				ts = append(ts, &TokenString{payload: token})
+				break
+			} else {
+				ts = append(ts, &TokenString{payload: token[:idx]})
+				token = token[idx:]
+			}
+		}
+		for _, legalToken := range legalTokens {
+			if strings.HasPrefix(token, legalToken) {
+				token = token[len(legalToken):]
+				switch legalToken {
+				case "%bar":
+					ts = append(ts, &TokenBar{})
+				case "%current":
+					ts = append(ts, &TokenCurrent{})
+				case "%total":
+					ts = append(ts, &TokenTotal{})
+				case "%percent":
+					ts = append(ts, &TokenPercent{})
+				case "%elapsed":
+					ts = append(ts, &TokenElapsed{})
+				case "%rate":
+					ts = append(ts, &TokenRate{})
+				}
+				break
+			}
+		}
+	}
+	return
+}
+
+func formatBarString(thisBar bar) (barString string) {
+	switch thisBar.(type) {
+	case *ProgressBar:
+		for _, token := range thisBar.(*ProgressBar).tokens {
+			barString += token.toString(thisBar)
+		}
+	case *UncertainProgressBar:
+		for _, token := range thisBar.(*UncertainProgressBar).tokens {
+			barString += token.toString(thisBar)
+		}
+	}
+	return
+}
--- progress_bar/progress_bar.go
+++ progress_bar/progress_bar.go
@@ -3,31 +3,57 @@ package progress_bar
 import (
 	"fmt"
 	"github.com/gngtwhh/gocui/cursor"
+	"github.com/gngtwhh/gocui/utils"
 	"github.com/gngtwhh/gocui/window"
 	"strings"
 	"sync"
 	"time"
 )
 
+// bar is a common interface for ProgressBar and UncertainProgressBar.
+type bar interface {
+	SetPos(posX, posY, width, height int)
+	Run(period time.Duration)
+	Print()
+	Stop()
+}
+
 // ProgressBar is a simple progress bar implementation.
 type ProgressBar struct {
 	Total, Current int
 	posX, posY     int
 	width, height  int
+	rate, elapsed  time.Duration
+	tokens         []Token
 	running        bool          // Whether the progress bar is running
 	interrupt      chan struct{} // interrupt channel to stop Run()
 	Done           chan struct{} // Done channel to signal completion
 	rw             sync.RWMutex  // RWMutex to synchronize access to the progress bar
 }
 
-// NewProgressBar creates a new progress bar with the given total.
-func NewProgressBar(total int) *ProgressBar {
-	return &ProgressBar{
+// // NewProgressBar creates a new progress bar with the given total.
+//
+//	func NewProgressBar(total int) *ProgressBar {
+//		return &ProgressBar{
+//			Total:     total,
+//			interrupt: make(chan struct{}),
+//			Done:      make(chan struct{}),
+//			rw:        sync.RWMutex{},
+//		}
+//	}
+
+// NewProgressBar creates a new progress bar that with the given style and total.
+func NewProgressBar(style, config string, total int) (pb *ProgressBar, err error) {
+	//styleTokens,configTokens := unmarshalToken(style), unmarshalToken(config)
+	styleTokens, _ := unmarshalToken(style), unmarshalToken(config)
+	pb = &ProgressBar{
 		Total:     total,
+		tokens:    styleTokens,
 		interrupt: make(chan struct{}),
 		Done:      make(chan struct{}),
 		rw:        sync.RWMutex{},
 	}
+	return pb, err
 }
 
 // SetPos sets the position of the progress bar.
@@ -54,26 +80,67 @@ func (p *ProgressBar) Update(current int) {
 // Print prints the current progress of the progress bar.
 func (p *ProgressBar) Print() {
 	p.rw.RLock()              // Read lock to avoid race condition
-	cursor.CursorMutex.Lock() // Lock the cursor to avoid concurrent access
+	utils.ConsoleMutex.Lock() // Lock the cursor to avoid concurrent access
+
+	//fmt.Printf("%s", cursor.HideCursor())
+	//window.ClearArea(p.posX, p.posY, p.width, p.height)
+	//for i := 0; i < p.height; i++ {
+	//	fmt.Printf("%s", cursor.GotoXY(p.posX+i, p.posY))
+	//	block := int(float64(p.Current) / float64(p.Total) * float64(p.width))
+	//	fmt.Printf("%s", strings.Repeat("█", block)+strings.Repeat(" ", p.width-block))
+	//}
+	//fmt.Printf("%s", cursor.ShowCursor())
+
+	var getParamAndToString = func(token Token) (payload string) {
+		switch token.(type) {
+		case *TokenBar:
+			block := int(float64(p.Current) / float64(p.Total) * float64(p.width))
+			left, right := 0, p.width-block
+			return token.toString(left, right, strings.Repeat("█", block))
+		case *TokenCurrent:
+			return token.toString(p.Current)
+		case *TokenTotal:
+			return token.toString(p.Total)
+		case *TokenPercent:
+			return token.toString(p.Current, p.Total)
+		case *TokenElapsed:
+			return token.toString(p.elapsed)
+		case *TokenRate:
+			return token.toString(p.rate)
+		case *TokenString:
+			return token.toString()
+		default:
+			return
+		}
+	}
 
 	fmt.Printf("%s", cursor.HideCursor())
 	window.ClearArea(p.posX, p.posY, p.width, p.height)
 	for i := 0; i < p.height; i++ {
 		fmt.Printf("%s", cursor.GotoXY(p.posX+i, p.posY))
-		block := int(float64(p.Current) / float64(p.Total) * float64(p.width))
-		fmt.Printf("%s", strings.Repeat("█", block)+strings.Repeat(" ", p.width-block))
+		payloadBuilder := strings.Builder{}
+		for _, token := range p.tokens {
+			payloadBuilder.WriteString(getParamAndToString(token))
+		}
+		fmt.Printf("%s", payloadBuilder.String())
 	}
-	cursor.CursorMutex.Unlock() // Unlock the cursor
+	//fmt.Printf("%s", cursor.ShowCursor())
+
+	utils.ConsoleMutex.Unlock() // Unlock the cursor
 	p.rw.RUnlock()              // Read unlock
 }
 
 // Run starts the progress bar.
 func (p *ProgressBar) Run(period time.Duration) {
+	p.rw.Lock()
+	p.rate = period
+	p.rw.Unlock()
 	if p.running {
 		return
 	}
 	ticker := time.NewTicker(period)
 	p.running = true
+	p.Print() // Print the initial state
 	go func() {
 		defer func() {
 			ticker.Stop()
@@ -89,6 +156,7 @@ func (p *ProgressBar) Run(period time.Duration) {
 				}
 				p.rw.Lock()
 				p.Current++
+				p.elapsed += period
 				p.rw.Unlock()
 			case <-p.interrupt:
 				return
@@ -104,10 +172,10 @@ func (p *ProgressBar) Stop() {
 }
 
 // UncertainProgressBar is a progress bar that shows an uncertain progress.
-
 type UncertainProgressBar struct {
 	posX, posY         int
 	width, height      int
+	tokens             []Token
 	Current, blockSize int
 	direction          int // 1 for increasing, -1 for decreasing
 	running            bool
@@ -137,19 +205,19 @@ func (p *UncertainProgressBar) SetPos(posX, posY, width, height int) {
 }
 
 func (p *UncertainProgressBar) Print() {
-	cursor.CursorMutex.Lock() // Lock the cursor to avoid concurrent access
+	utils.ConsoleMutex.Lock() // Lock the cursor to avoid concurrent access
 	fmt.Printf("%s", cursor.HideCursor())
 	window.ClearArea(p.posX, p.posY, p.width, p.height)
 	fmt.Printf("%s", cursor.GotoXY(p.posX, p.posY))
 
 	leftSpace := p.Current
 	rightSpace := p.width - 2 - leftSpace - p.blockSize
-	bar := "[" + strings.Repeat(" ", leftSpace) + strings.Repeat("█", p.blockSize) + strings.Repeat(" ", rightSpace) + "]"
+	bar := "[" + strings.Repeat(" ", leftSpace) + strings.Repeat("#", p.blockSize) + strings.Repeat(" ", rightSpace) + "]"
 	for i := 0; i < p.height; i++ {
 		fmt.Printf("%s", cursor.GotoXY(p.posX+i, p.posY))
 		fmt.Printf("%s", bar)
 	}
-	cursor.CursorMutex.Unlock() // Unlock the cursor
+	utils.ConsoleMutex.Unlock() // Unlock the cursor
 }
 
 func (p *UncertainProgressBar) update() {
