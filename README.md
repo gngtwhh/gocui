04d61fb6d24aedcc0b155e575924f920284549cd
gngtwhh <2784126850@qq.com>
2024-06-28 18:04:58 +0800

    test: 修改main.go测试用例

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Tokens that users use to customize the style of the objects.
 
 ### progress bar
 - `%bar`: the progress bar
-- `current`: the current value
-- `total`: the total value
+- `%current`: the current value
+- `%total`: the total value
 - `%percent`: the percentage
 - `%elapsed`: the elapsed time
 - `%rate`: Interval between two updates
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/gngtwhh/gocui/box"
+	"github.com/gngtwhh/gocui/cursor"
+	"github.com/gngtwhh/gocui/graph"
 	"github.com/gngtwhh/gocui/progress_bar"
 	"github.com/gngtwhh/gocui/window"
+	"math"
 	"time"
 )
 
-func main() {
+func boxTest() {
 	/*payload := []string{
 		"                       图书管理系统        ",
 		"",
@@ -32,14 +35,81 @@ func main() {
 	window.ClearScreen()
 	aBox, _ := box.GetBox(len(payload)+2, 50+2, "bold", payload)
 	box.SetBoxAt(aBox, 0, 0)
+}
 
-	p := progress_bar.NewProgressBar(100)
-	p.SetPos(10, 0, 52, 1)
-	/*for i := 0; i < 100; i++ {
-		p.Update(i)
-		time.Sleep(time.Millisecond * 100)
-	}*/
+func barTest() {
+	window.ClearScreen()
+	// test progress bar
+	p, _ := progress_bar.NewProgressBar("[%bar] %current/%total-%percent %rate", "", 100)
+	p.SetPos(0, 0, 52, 1)
 	p.Run(time.Millisecond * 100)
+
+	// test uncertain progress bar
+	//up := progress_bar.NewUncertainProgressBar()
+	//up.SetPos(11, 0, 52, 1)
+	//up.Run(time.Millisecond * 100)
+
+	// wait
 	<-p.Done
-	fmt.Println("\ntime out. exit...")
+	//up.Stop()
+	fmt.Printf("%s", cursor.GotoXY(1, 0))
+	fmt.Println("time out. exit...")
+}
+
+func lineTest() {
+	drawCoord := func(x, y, length int) {
+		graph.Line(x, y, length, '|', 0)
+		fmt.Printf("%s", cursor.GotoXY(x+length, y)+"v")
+		length = 60
+		graph.Line(x, y, length, '-', 1)
+		fmt.Printf("%s", cursor.GotoXY(x, y+length)+">")
+		fmt.Printf("%s", cursor.GotoXY(x, y)+"+")
+	}
+
+	var x, y, length int
+
+	window.ClearScreen()
+
+	// curve f(x)=x^2
+	x, y, length = 1, 2, 10
+	drawCoord(x, y, length)
+	f := func(x int) int {
+		return int(math.Pow(float64(x), 2))
+	}
+	x, y, length = 1, 2, 10
+	graph.Curve(x, y, length, 1, '*', f)
+
+	//time.Sleep(time.Second * 2)
+	window.ClearScreen()
+
+	// curve f(x)=sin(x)
+	x, y, length = 2, 30, 10
+	drawCoord(x, y, length)
+	f = func(x int) int {
+		return int(math.Sin(float64(x)) * 5)
+	}
+	x, y, length = 2, 30, 10
+	graph.Curve(x, y, length, 1, '*', f)
+
+	//time.Sleep(time.Second * 2)
+	window.ClearScreen()
+
+	// curve f(x)=sqrt(3)*x
+	x, y, length = 10, 30, 20
+	drawCoord(x, y, length)
+	f = func(x int) int {
+		return int(math.Sqrt(3) * float64(x))
+	}
+	x, y, length = 10, 30, 15
+	graph.Curve(x, y, length, 1, '*', f)
+	graph.Curve(x, y, length, -1, '*', f)
+
+}
+func main() {
+	//boxTest()
+	barTest()
+	//window.ClearScreen()
+	//lineTest()
+	c := '0'
+	_, _ = fmt.Scanf("%c", &c)
 }
